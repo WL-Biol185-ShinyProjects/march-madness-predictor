766df4e93596b0f99262c7e734de428c21b34637
Diya Shreenath <shreenathd24@mail.wlu.edu>
2023-10-28 15:55:42 -0400

    first part of our app

--- server.R
+++ server.R
@@ -1,11 +1,48 @@
 library(shiny)
+library(leaflet)
+library(tidyverse)
 
+#Define server logic for shiny app
 server <- function(input, output) {
   
+  #Read the data
+  march_madness_data <- read_csv("~/march-madness-predictor/data/Bio_185_March_Madness_Data.csv")
   
+  #Data manipulation
+  march_madness_data <- march_madness_data %>%
+    rename(lat = `Lat for City Where Game was played`,
+           lng = `Lon for City Where Game was Played`,
+           distance_favorite = `Distance to Favorite`,
+           game_city = `City Where Game Was Played`,
+           game_state = `State Where Game was Played`)
+  
+  game_total <- march_madness_data %>%
+    count(lat, lng, game_city)
+  
+  game_total$popup_text <- paste0(game_total$game_city, ": ", game_total$n, " games")
+  
+  output$map <- renderLeaflet({
+    leaflet(data = game_total) %>%
+      setView(lng = -96.25, lat = 39.5, zoom = 4) %>%
+      addTiles()%>%
+      addMarkers(popup = ~game_city, label = ~popup_text)
+  })
 }
 
-shinyApp(ui, server)
+#Run the shiny app
+#shinyApp(ui, server)
+
+
+
+
+
+
+#server <- function(input, output) {
+  
+  
+#}
+
+#shinyApp(ui, server)
 
 
 
--- ui.R
+++ ui.R
@@ -1,12 +1,24 @@
 library(shiny)
 library(tidyverse)
 library(ggplot2)
+library(leaflet)
 
+#Define UI for the shiny app
 ui <- fluidPage(
-  titlePanel("title panel"),
-  
-  sidebarLayout(
-    sidebarPanel("sidebar panel"),
-    mainPanel("main panel")
+    titlePanel("March Madness Game Locations"),
+    leafletOutput("map")
   )
-)
+
+
+
+
+
+
+#ui <- fluidPage(
+  #titlePanel("title panel"),
+  
+  #sidebarLayout(
+   # sidebarPanel("sidebar panel"),
+   # mainPanel("main panel")
+ # )
+#)

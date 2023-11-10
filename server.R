6b1a5358d0f0cdc6ddc4d62d2a0ee44ede43e46b
Diya Shreenath <shreenathd24@mail.wlu.edu>
2023-11-10 15:41:00 -0500

    now this has 3 tabs on the shiny app!

--- server.R
+++ server.R
@@ -32,7 +32,7 @@ server <- function(input, output) {
 
 #Code for Tab 2
 
-#read the data 
+#read the data
 final_four_teams <- read_csv("~/march-madness-predictor/data/Final Four Teams.csv")
 conference_data <- read_csv("~/march-madness-predictor/data/mach_madness_conference_list.csv")
 
@@ -42,19 +42,110 @@ new_data <- final_four_teams %>%
 new_data[is.na(new_data)] <- "Non Power 6"
 
 #Calculate and Display the probabilities
-data_count <- reactive({
+filtered_data <- reactive({
   new_data %>%
+    filter(Conference == input$conference)
+})
+
+output$data_table <- renderTable({
+  data_count <- filtered_data() %>%
     select(Year, Conference) %>%
     count(Conference, Year) %>%
     group_by(Conference, n) %>%
     count() %>%
     mutate(prob_occurence = nn / 83)
+  return(data_count)
 })
 
-output$data_table <- renderTable({
-  data_count()
-})
+#Code for Tab 3
+#Load Data
+distance_data <- read_csv("~/march-madness-predictor/data/Bio 185 March Madness Data Lats.csv")
+
+#Calculate distances using haversine
+haversine <- function(long1, lat1, long2, lat2, round = 3) {
+  # convert to radians
+  long1 = long1 * pi / 180
+  lat1  = lat1  * pi / 180
+  long2 = long2 * pi / 180
+  lat2  = lat2  * pi / 180
+
+  R = 6371 # Earth mean radius in km
+
+  a = sin((lat2 - lat1)/2)^2 + cos(lat1) * cos(lat2) * sin((long2 - long1)/2)^2
+  d = R * 2 * asin(sqrt(a))
+
+  return( round(d,round) ) # distance in km
 }
 
+distance_data <- distance_data %>%
+  mutate(distance_favorite = haversine(College_lon, College_lat, `Lon for City Where Game was Played`, `Lat for City Where Game was played`, round = 0))
 
+distance_data <- distance_data %>%
+  mutate(distance_underdog = haversine(Underdog_lon, Underdog_lat, `Lon for City Where Game was Played`, `Lat for City Where Game was played`, round = 0))
 
+distance_data <- distance_data %>%
+  select(Year, Favorite, Underdog, distance_favorite, distance_underdog, Round)
+
+favorite_data_table <- distance_data %>%
+  select(Year, Favorite, distance_favorite, Round) %>%
+  group_by(Favorite, Year) %>%
+  mutate(total_distance_favorite = sum(distance_favorite))
+
+underdog_data_table <- distance_data %>%
+  select(Year, Underdog, distance_underdog, Round) %>%
+  group_by(Underdog, Year) %>%
+  mutate(total_underdog_distance = sum(distance_underdog))
+
+favorite_data_table <- favorite_data_table %>%
+  mutate(team_name = Favorite)
+
+underdog_data_table <- underdog_data_table %>%
+  mutate(team_name = Underdog)
+
+total_team_distance_traveled <- full_join(favorite_data_table, underdog_data_table, by = c("Year" = "Year", "team_name" = "team_name"))
+
+total_team_distance_traveled <- total_team_distance_traveled %>%
+  mutate(total_team_travel = (total_distance_favorite + total_underdog_distance))
+
+total_team_distance_traveled$total_underdog_distance[is.na(total_team_distance_traveled$total_underdog_distance)] <- 0
+total_team_distance_traveled$total_distance_favorite[is.na(total_team_distance_traveled$total_distance_favorite)] <- 0
+total_team_distance_traveled$total_team_travel[is.na(total_team_distance_traveled$total_team_travel)] <- 0
+
+#Filter out duplicate rows
+total_team_distance_traveled <- total_team_distance_traveled %>%
+  group_by(team_name, Year) %>%
+  mutate(kilometers_traveled = pmax(total_distance_favorite, total_underdog_distance, total_team_travel))
+
+total_team_distance_traveled %>%
+  select(Year, team_name, kilometers_traveled,)
+
+total_team_distance_traveled <- unique(total_team_distance_traveled[, c("team_name", "Year", "kilometers_traveled")])
+
+total_team_distance_traveled <- total_team_distance_traveled %>%
+  inner_join(final_four_teams, by = c("Year" = "Year", "team_name" = "Team"))
+
+#Convert kilometers to miles
+total_team_distance_traveled <- total_team_distance_traveled %>%
+  mutate(miles_traveled = kilometers_traveled * .621371)
+
+#display data table
+  output$data_table_distances <- renderTable({
+    team_name <- input$team_select
+    filtered_data <- total_team_distance_traveled[total_team_distance_traveled$team_name == team_name, ]
+    return(filtered_data)
+  })
+
+#Display a box chart for the total distance traveled
+  output$box_chart <- renderPlot({
+    team_name <- input$team_select
+    filtered_data <- total_team_distance_traveled[total_team_distance_traveled$team_name == team_name, ]
+    
+    ggplot(filtered_data, aes(x = Year, y = miles_traveled)) +
+      geom_boxplot(fill = "lightblue", color = "blue") +
+      labs(
+        title = paste("Total Distance Traveled by", team_name),
+        x = "Year",
+        y = "Miles Traveled"
+      )
+  })
+}
--- ui.R
+++ ui.R
@@ -1,3 +1,4 @@
+#load packages
 library(shiny)
 library(tidyverse)
 library(ggplot2)
@@ -6,21 +7,29 @@ library(leaflet)
 #Define UI for the shiny app
 ui <- fluidPage(
     titlePanel("March Madness Predictor"),
-    navbarPage("Tabs",
+    
+#set up tabs    
+    navbarPage(
+      "Tabs",
        tabPanel("Game Locations",
         leafletOutput("map"),
           textOutput("text1")
                ),
-#UI for Tab 2       
-    tabPanel("Final Four Probability by Conference",
-    sidebarLayout(
-      sidebarPanel(
-        selectInput("conference", "Select Conference", choices = unique(new_data$Conference)),
-      ),
-      mainPanel(
-        tableOutput("data_table")
-      )
-    )
-)))       
-        
+#UI for Tab 2
+      tabPanel("Final Four Probability by Conference",
+          selectInput("conference", "Select Conference", 
+              choices = unique(new_data$Conference)),
+          tableOutput("data_table"),
+          textOutput("text2")
+  ),
 
+#UI for Tab 3
+    tabPanel("Distances",
+      selectInput("team_select", "Select a Team That Has Made It To The Final Four In The Last 10 Years", 
+          choices = unique(total_team_distance_traveled$team_name)
+        ),
+      plotOutput("box_chart"),
+      tableOutput("data_table_distances")
+    )
+  ) 
+)
